0e2007a5b3232ea3ca21dbf8616d82a2ff664946
setzer22 <jsanchezfsms@gmail.com>
2024-03-09 22:26:26 +0100

    Add Ghost (by @IrisT!) and make it blink

--- scenes/Player/Player.cs
+++ scenes/Player/Player.cs
@@ -1,11 +1,13 @@
 using System;
+using System.Runtime.Versioning;
 using Godot;
 
 public class Player : KinematicBody2D
 {
     private float elapsed = 0f;
     private Vector2 velocity = new Vector2();
-
+    private float timeToNextBlink = 0f;
+    
     // Store the initial sprite translation so that we can add an offset to it
     private Vector2 initialSpriteTranslation;
 
@@ -17,11 +19,19 @@ public class Player : KinematicBody2D
 
     [Export]
     private float MoveSpeed = 150f;
+    
+    [Export]
+    private float BlinkMin = 2.5f;
+    
+    [Export]
+    private float BlinkMax = 8f;
+
+    
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        initialSpriteTranslation = GetNode<Sprite>("Sprite").Position;
+        initialSpriteTranslation = GetNode<Node2D>("Sprite").Position;
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -30,16 +40,40 @@ public class Player : KinematicBody2D
         elapsed += delta;
 
         // Float animation
-        var sprite = GetNode<Sprite>("Sprite");
+        var sprite = GetNode<Node2D>("Sprite");
         sprite.Position =
             initialSpriteTranslation
             + Vector2.Up * Mathf.Sin(elapsed * FloatAnimFrequency) * FloatAnimAmplitude;
-        
+
         // Flip sprite towards movement direction
         if (velocity.x != 0)
         {
-            sprite.FlipH = velocity.x > 0;
+            foreach (var ch in sprite.GetChildren())
+            {
+                if (ch is AnimatedSprite s)
+                {
+                    s.FlipH = velocity.x > 0;
+                }
+            }
         }
+
+        if (timeToNextBlink > 0)
+        {
+            timeToNextBlink -= delta;
+        }
+        else
+        {
+            Blink();
+            timeToNextBlink = (float)GD.RandRange(BlinkMin, BlinkMax);
+        }
+    }
+
+    // Don't
+    public void Blink()
+    {
+        var eyes = GetNode<AnimatedSprite>("Sprite/Eyes");
+        eyes.Frame = 0;
+        eyes.Playing = true;
     }
 
     public override void _PhysicsProcess(float delta)
