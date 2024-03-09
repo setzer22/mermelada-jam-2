24641cbd66121e00e2387f300e8bbdd75b5b3e9a
rodgal-javier <rodgal.javier@gmail.com>
2024-03-09 11:50:44 +0100

    menu Scene working

--- scripts/UI/MainMenu.gd
+++ scripts/UI/MainMenu.gd
@@ -6,11 +6,9 @@ export var mainGameScene : PackedScene
 func _ready():
 	$VBoxContainer/VBoxContainer/StartButton.grab_focus()
 
-func _on_StartGameButton_pressed():
+func _on_StartButton_pressed():
 	get_tree().change_scene(mainGameScene.resource_path)
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
 
-func _on_StartButton_pressed():
-	pass # Replace with function body.
